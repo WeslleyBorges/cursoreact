91ad4a7961e5adeeacbe4399764220c048966e70
Weslley Borges <weslleynfs@gmail.com>
2019-10-28 16:26:24 -0400

    Ajustes finais de apresentação da calculadora. Esperando apenas pela implementação da lógica

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -1,4 +1,17 @@
 import React from 'react'
 import './Button.css'
 
-export default props => <button className="button">{props.label}</button>
+export default props => {
+  let classes = 'button '
+  classes += props.operator ? 'operator' : ''
+  classes += props.double ? 'double' : ''
+  classes += props.triple ? 'triple' : ''
+
+  return (
+    <button 
+      className={classes}
+      onClick={_ => props.click && props.click(props.label)}>
+      {props.label}
+    </button>
+  )
+}
--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -4,27 +4,51 @@ import Button from '../components/Button'
 import Display from '../components/Display'
 
 export default class Calculator extends Component {
+    // constructor(props) {
+    //     super(props)
+
+    //     this.cleanDisplay = this.cleanDisplay.bind(this)
+    //     this.addDigit = this.addDigit.bind(this)
+    //     this.setOperation = this.setOperation.bind(this)
+    // }
+
+    cleanDisplay() {
+        console.log('limpou')
+    }
+
+    setOperation(op) {
+        console.log(op)
+    }
+
+    addDigit(n) {
+        console.log(n)
+    }
+    
     render(){
+        const addDigit = n => this.addDigit(n)
+        const setOperation = op => this.setOperation(op)
+        const cleanDisplay = _ => this.cleanDisplay() 
         return (
             <div className="calculator">
-                <Display value={10}></Display>
-                <Button label="AC"></Button>
-                <Button label="/"></Button>
-                <Button label="7"></Button>
-                <Button label="8"></Button>
-                <Button label="9"></Button>
-                <Button label="*"></Button>
-                <Button label="4"></Button>
-                <Button label="5"></Button>
-                <Button label="6"></Button>
-                <Button label="-"></Button>
-                <Button label="1"></Button>
-                <Button label="2"></Button>
-                <Button label="3"></Button>
-                <Button label="+"></Button>
-                <Button label="0"></Button>
-                <Button label="."></Button>
-                <Button label="="></Button>                
+                <Display value={10000}></Display>
+                <Button label="AC" triple
+                    click={cleanDisplay}></Button>
+                <Button label="/" click={setOperation} operator></Button>
+                <Button label="7" click={addDigit}></Button>
+                <Button label="8" click={addDigit}></Button>
+                <Button label="9" click={addDigit}></Button>
+                <Button label="*" click={setOperation} operator></Button>
+                <Button label="4" click={addDigit}></Button>
+                <Button label="5" click={addDigit}></Button>
+                <Button label="6" click={addDigit}></Button>
+                <Button label="-" click={setOperation} operator></Button>
+                <Button label="1" click={addDigit}></Button>
+                <Button label="2" click={addDigit}></Button>
+                <Button label="3" click={addDigit}></Button>
+                <Button label="+" click={setOperation} operator></Button>
+                <Button label="0" click={addDigit} double></Button>
+                <Button label="." click={addDigit}></Button>
+                <Button label="=" click={setOperation} operator></Button>                
             </div>
         )
     }

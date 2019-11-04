968895f8937f7cbbaf28d3dd04be7e72623ed7b1
Weslley Borges <weslleynfs@gmail.com>
2019-11-04 13:45:33 -0400

    Progresso curso react

--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -7,7 +7,7 @@ export default props => (
     <div role="form" className="todoForm">
         <Grid cols="12 9 10">
            <input type="text" id="description" className="form-control"
-                placeholder="Adicione uma tarefa"/> 
+                placeholder="Adicione uma tarefa" onChange={props.handleChange}/> 
         </Grid>
         <Grid cols="12 3 2">
             <ButtonIcon onClick={props.handleAdd} style="primary" icon="plus"></ButtonIcon>

1da305b8969467895cb317e43c4cecd4a9bb46ce
Weslley <weslleynfs@gmail.com>
2019-11-03 22:23:30 -0400

    Progresso curso react

---
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Grid from '../template/Grid'
+import ButtonIcon from '../template/ButtonIcon'
+
+export default props => (
+    <div role="form" className="todoForm">
+        <Grid cols="12 9 10">
+           <input type="text" id="description" className="form-control"
+                placeholder="Adicione uma tarefa"/> 
+        </Grid>
+        <Grid cols="12 3 2">
+            <ButtonIcon onClick={props.handleAdd} style="primary" icon="plus"></ButtonIcon>
+        </Grid>
+    </div>
+)

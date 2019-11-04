8f63a73d219d8aaf3041c2f983af01dfb83b3f4f
Weslley Borges <weslleynfs@gmail.com>
2019-11-04 19:38:07 -0400

    progresso curso react

--- todo-app/backend_drf/backend_drf/settings.py
+++ todo-app/backend_drf/backend_drf/settings.py
@@ -78,18 +78,18 @@ WSGI_APPLICATION = 'backend_drf.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django_mongodb_engine',
+        'NAME': 'todoapp',
+    }
+}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
